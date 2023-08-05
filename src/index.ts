5647973759aa751987ae3bd43b87811e0298765d
Nathaniel Richards <nthnlrichards@gmail.com>
2023-08-05 02:08:32 -0400

    Added middleware support

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,16 @@ import http from 'http';
 import { PulseConfig } from './config';
 import Logger from '@ptkdev/logger';
 
-export type PulseHandler = (req: http.IncomingMessage, res: http.ServerResponse) => void;
+export type PulseHandler = (req: http.IncomingMessage, res: http.ServerResponse, next?: () => void) => void;
 
 export type PulseError = {};
 
 class PulseServer {
   private server: http.Server;
   private config: PulseConfig;
-  private routes: Record<string, Record<string, PulseHandler>>;
+  private routes: Record<string, Record<string, PulseHandler[]>>;
   private logger: Logger;
+  private middlewares: PulseHandler[] = [];
 
   constructor(config: PulseConfig) {
     this.routes = {};
@@ -21,46 +22,70 @@ class PulseServer {
     this.logger.sponsor('Thank you for using Pulse!');
 
     this.server = http.createServer((req, res) => {
-      const handler = this.routes[req.url!][req.method!] || this.routeFallback;
+      const matchedRoutes = this.routes[req.url!];
+      const matchedMethod = matchedRoutes ? matchedRoutes[req.method!] : null;
+      const handler = matchedMethod && matchedMethod.length ? matchedMethod : [this.routeFallback];
 
-      handler(req, res);
-      this.loggerMiddleware(req, res, handler);
+      const handlers = [...this.middlewares, ...handler];
+      this.handle(req, res, handlers);
     });
+
+    if (config.usePulseLogger) {
+      this.use(this.loggerMiddleware);
+    }
   }
+
+  private handle(req: http.IncomingMessage, res: http.ServerResponse, handlers: PulseHandler[]) {
+    const next = () => {
+      const handler = handlers.shift();
+      if (handler) handler(req, res, next);
+    };
+    next();
+  }
+
+  public use(handler: PulseHandler) {
+    this.middlewares.push(handler);
+  }
+
+  private loggerMiddleware: PulseHandler = (req, res, next) => {
+    this.logger.info(req.method + ' Request To Route:' + req.url);
+    if (next) next();
+  };
 
   public get(path: string, handler: PulseHandler) {
     if (!this.routes[path]) {
       this.routes[path] = {};
     }
-
-    this.routes[path]['GET'] = handler;
+    if (!this.routes[path]['GET']) {
+      this.routes[path]['GET'] = [];
+    }
+    this.routes[path]['GET'].push(handler);
   }
 
   public post(path: string, handler: PulseHandler) {
     if (!this.routes[path]) {
       this.routes[path] = {};
     }
-
-    this.routes[path]['POST'] = handler;
+    if (!this.routes[path]['POST']) {
+      this.routes[path]['POST'] = [];
+    }
+    this.routes[path]['POST'].push(handler);
   }
 
   public delete(path: string, handler: PulseHandler) {
     if (!this.routes[path]) {
       this.routes[path] = {};
     }
-
-    this.routes[path]['DELETE'] = handler;
+    if (!this.routes[path]['DELETE']) {
+      this.routes[path]['DELETE'] = [];
+    }
+    this.routes[path]['DELETE'].push(handler);
   }
 
   private routeFallback(req: http.IncomingMessage, res: http.ServerResponse) {
     res.statusCode = 400;
     this.logger.error(`Bad Request: ${req.url}`);
     res.end('Bad Request');
-  }
-
-  private loggerMiddleware(req: http.IncomingMessage, res: http.ServerResponse, next: PulseHandler) {
-    this.logger.info(req.method + ' Request To Route:' + req.url);
-    next(req, res);
   }
 
   start(callback: () => void) {
